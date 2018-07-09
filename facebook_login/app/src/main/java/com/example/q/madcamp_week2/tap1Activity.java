015b07e31c1a49d43cbaec2a69293558924a5372
LaLlorona <40677863+LaLlorona@users.noreply.github.com>
2018-07-09 18:59:08 +0900

    ServerUpload
    implementation completed

--- facebook_login/app/src/main/java/com/example/q/madcamp_week2/tap1Activity.java
+++ facebook_login/app/src/main/java/com/example/q/madcamp_week2/tap1Activity.java
@@ -5,10 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,9 +22,23 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
 public class tap1Activity extends AppCompatActivity {
     ListView listView;
     TextView textView;
+
+    private static final int GALLERY_MODE = 10;
+    private FirebaseStorage storage;
+
+
+
 
 
 
@@ -30,10 +48,22 @@ public class tap1Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tap1_layout);
 
+        storage = FirebaseStorage.getInstance();
+
+
         Button tap1 = (Button)findViewById(R.id.act1_tap1_btn);
         Button tap2 = (Button)findViewById(R.id.act1_tap2_btn);
         Button tap3 = (Button)findViewById(R.id.act1_tap3_btn);
+        Button open_btn = (Button)findViewById(R.id.open_btn);
 
+        open_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Intent.ACTION_PICK);
+                myIntent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(myIntent,GALLERY_MODE);
+            }
+        });
 
 
         tap2.setOnClickListener(new View.OnClickListener() {
@@ -54,6 +84,55 @@ public class tap1Activity extends AppCompatActivity {
                 finish();
             }
         });
+
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GALLERY_MODE) {
+
+
+
+            System.out.println(getPath(data.getData()));
+
+            StorageReference storageRef = storage.getReference();
+
+            Uri file = Uri.fromFile(new File(getPath(data.getData())));
+            StorageReference riversRef = storageRef.child("images/"+file.getLastPathSegment());
+            UploadTask uploadTask = riversRef.putFile(file);
+
+// Register observers to listen for when the download is done or if it fails
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Toast.makeText(getApplicationContext(),"업로드에 실패했습니다.",Toast.LENGTH_SHORT).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                    Toast.makeText(getApplicationContext(),"업로드에 성공했습니다.",Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+    }
+    public String getPath(Uri uri){
+        String [] proj = {MediaStore.Images.Media.DATA};
+        CursorLoader cursorLoader = new CursorLoader(this,uri,proj,null,null,null);
+        Cursor cursor = cursorLoader.loadInBackground();
+        int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+
+        return cursor.getString(index);
+
+    }
+
+
+
+
+
+
 }
 
