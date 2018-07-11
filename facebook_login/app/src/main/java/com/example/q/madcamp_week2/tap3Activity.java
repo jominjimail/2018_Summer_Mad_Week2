a7be160c9dea1a83ef765baf26867e522935742a
LaLlorona <40677863+LaLlorona@users.noreply.github.com>
2018-07-11 20:23:56 +0900

    tab3_implementing
    1

--- facebook_login/app/src/main/java/com/example/q/madcamp_week2/tap3Activity.java
+++ facebook_login/app/src/main/java/com/example/q/madcamp_week2/tap3Activity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +23,7 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -28,23 +31,27 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
 public class tap3Activity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
-    private CallbackManager callbackManager;
-    Elements contents;
-    Document doc = null;
-    String Top10;//결과를 저장할 문자열변수
-    String tempName;
+    private TextView tv;
+    // 웹사이트 주소를 저장할 변수
+    String urlAddress ;
+    Handler handler = new Handler(); // 화면에 그려주기 위한 객체
+    EditText master;
+    EditText repo;
 
 
     LoginButton facebook_login;
-    EditText master;
-    EditText repo;
+
 
     TextView textView;
 
@@ -54,85 +61,30 @@ public class tap3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tap3_layout);
 
+        FirebaseInstanceId.getInstance().getToken();
 
-        Button tap1 = (Button) findViewById(R.id.act3_tap1_btn);
-        Button tap2 = (Button) findViewById(R.id.act3_tap2_btn);
-        Button tap3 = (Button) findViewById(R.id.act3_tap3_btn);
-        Button parse_btn = (Button) findViewById(R.id.parse_button);
-        master = (EditText) findViewById(R.id.master_name);
-        repo = (EditText) findViewById(R.id.repo_name);
-        textView = (TextView) findViewById(R.id.textBox);
-        final ArrayList nameArrayList = new ArrayList<String>();
-        final ArrayList commitNum = new ArrayList<Integer>();
-
+        if (FirebaseInstanceId.getInstance().getToken() != null) {
+            Log.d(TAG, "token = " + FirebaseInstanceId.getInstance().getToken());
+        }
 
         /*
 
 
-        try {
-            //https://github.com/jominjimail/2018_Summer_Mad_Week2/graphs/contributors
-
-
-            Document rawData = Jsoup.connect("https://www.naver.com/").get();
-
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "connection failed", Toast.LENGTH_SHORT).show();
-        }
+        Button tap1 = (Button) findViewById(R.id.act3_tap1_btn);
+        Button tap2 = (Button) findViewById(R.id.act3_tap2_btn);
+        Button tap3 = (Button) findViewById(R.id.act3_tap3_btn);
         */
-
-        parse_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new AsyncTask() {//AsyncTask객체 생성
-                    @Override
-                    protected Object doInBackground(Object[] params) {
-                        try {
-                            nameArrayList.clear();
-                            commitNum.clear();
-
-                            doc = Jsoup.connect("https://github.com/"+master.getText().toString()+"/"+repo.getText().toString()+"/commits/master").get(); //naver페이지를 불러옴
-                            contents = doc.select("a.commit-author.tooltipped.tooltipped-s.user-mention");//셀렉터로 span태그중 class값이 ah_k인 내용을 가져옴
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        Top10 = "";
-                        int cnt = 0;//숫자를 세기위한 변수
-                        for(Element element: contents) {
-                            cnt++;
-                            tempName = element.text();
-                            if (!nameArrayList.contains(tempName)){
-                                nameArrayList.add(tempName);
-                                commitNum.add(1);
-                            }
-                            else{//name is already in the nameArrayList
-                                int indexN = nameArrayList.indexOf(tempName);
-                                commitNum.set(indexN,(Integer)commitNum.get(indexN)+1);
+        //Button parse_btn = (Button) findViewById(R.id.parse_button);
+        master = (EditText) findViewById(R.id.master_text);
+        repo = (EditText) findViewById(R.id.repo_text);
+        textView = (TextView) findViewById(R.id.textView1);
 
 
 
-                            }
-                            /*
+        final ArrayList nameArrayList = new ArrayList<String>();
+        final ArrayList commitNum = new ArrayList<Integer>();
 
-
-                            Top10 += cnt+". "+element.text() + "\n";
-                            if(cnt == 100)//10위까지 파싱하므로
-                                break;
-                                */
-                        }
-                        for (int i=0;i<nameArrayList.size();i++){
-                            Top10 += "Uploader "+nameArrayList.get(i) +"'s total commits are  "+ Integer.toString((Integer)commitNum.get(i))+"\n";
-                        }
-                        return null;
-                    }
-                    @Override
-                    protected void onPostExecute(Object o) {
-                        super.onPostExecute(o);
-                        Log.i("TAG",""+Top10);
-                        textView.setText(Top10);
-                    }
-                }.execute();
-            }
-        });
+        /*
 
 
         tap1.setOnClickListener(new View.OnClickListener() {
@@ -156,45 +108,100 @@ public class tap3Activity extends AppCompatActivity {
                 finish();
             }
         });
+        */
+        master = (EditText) findViewById(R.id.master_text);
+        repo = (EditText)findViewById(R.id.repo_text);
 
 
-        //facebook
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
-        callbackManager = CallbackManager.Factory.create();
+        tv = (TextView)findViewById(R.id.textView1);
+        tv.setMovementMethod(ScrollingMovementMethod.getInstance());
+        Button b = (Button)findViewById(R.id.button1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadHtml(); // 웹에서 html 읽어오기
+            }
+        });
+    } // end of onCreate
 
-        facebook_login = findViewById(R.id.facebook_login);
+    void loadHtml() { // 웹에서 html 읽어오기
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuffer sb = new StringBuffer();
 
-        facebook_login.setReadPermissions("email");
-
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        // App code
-                        Log.d(TAG, "onSucces LoginResult=" + loginResult);
+                try {
+                    urlAddress = "https://api.github.com/repos/" + master.getText().toString() + "/" + repo.getText().toString() + "/contributors";
+                    URL url = new URL(urlAddress);
+                    HttpURLConnection conn =
+                            (HttpURLConnection) url.openConnection();// 접속
+                    if (conn != null) {
+                        conn.setConnectTimeout(2000);
+                        conn.setUseCaches(false);
+                        if (conn.getResponseCode()
+                                == HttpURLConnection.HTTP_OK) {
+                            //    데이터 읽기
+                            BufferedReader br
+                                    = new BufferedReader(new InputStreamReader
+                                    (conn.getInputStream(), "euc-kr"));//"utf-8"
+                            while (true) {
+                                String line = br.readLine();
+                                if (line == null) break;
+                                sb.append(line + "\n");
+                            }
+                            br.close(); // 스트림 해제
+                        }
+                        conn.disconnect(); // 연결 끊기
                     }
+                    // 값을 출력하기
+                    Log.d("test", sb.toString());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String temp = sb.toString();
+                            String[] users = temp.split("[,\\{}]");
+                            String parsed_data = "";
 
-                    @Override
-                    public void onCancel() {
-                        // App code
-                        Log.d(TAG, "onCancel");
-                    }
+                            //tv.setText(users[0]);
+                            ArrayList<String> userArray = new ArrayList<>();
 
-                    @Override
-                    public void onError(FacebookException exception) {
-                        // App code
-                        Log.d(TAG, "onError");
-                    }
-                });
+                            for (String temp1 : users) {
 
+                                userArray.add(temp1);
+
+
+                            }
+                            for (int i = 0; i < userArray.size(); i++) {
+                                if (userArray.get(i).contains("login")) {
+                                    parsed_data += "유저 ";
+
+                                    parsed_data += userArray.get(i).substring(9, userArray.get(i).length() - 1);
+
+                                    parsed_data += " 는 지금까지 ";
+
+
+                                } else if (userArray.get(i).contains("contributions")) {
+                                    parsed_data += userArray.get(i).substring(16);
+                                    parsed_data += "개의 commit 을 올렸습니다.";
+                                    parsed_data += "\n\n";
+                                }
+
+                            }
+                            tv.setText(parsed_data);
+
+
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start(); // 쓰레드 시작
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
+
+
 
 
 }
